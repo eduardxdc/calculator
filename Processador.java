1145249901a6e99c0fe443686aa2138562bcb1c5
eduardxdc <eduardxdc@proton.me>
2024-04-29 13:57:56 -0300

    initial commit

---
+++ Processador.java
@@ -0,0 +1,18 @@
+public class Processador {
+
+    public Integer adicao(int valor1, int valor2){
+        return valor1 + valor2;
+    }
+
+    public Integer subtracao(int valor1, int valor2){
+        return valor1 - valor2;
+    }
+
+    public Integer divisao(int valor1, int valor2){
+        return valor1 / valor2;
+    }
+
+    public Integer multiplicacao(int valor1, int valor2){
+        return valor1 * valor2;
+    } 
+}
